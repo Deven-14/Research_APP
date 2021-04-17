ca3ea7492ce148fd67d37f8a5b08daab435b4f81
sumedhdkme19 <sumedhdk.me19@bmace.ac.in>
2021-04-18 01:55:34 +0530

    Async Handled, need to write into CSV

--- list_activities_new.js
+++ list_activities_new.js
@@ -1,6 +1,6 @@
 const LineReaderSync = require("line-reader-sync");
 
-function link_student_id(email_ID, callback) {
+function link_student_id(email_ID, callback, superman, writer) {
   console.log("Student Email ID: " + email_ID);
   var lrs1 = new LineReaderSync("student_email_name_id.txt");
   while (true) {
@@ -14,17 +14,31 @@ function link_student_id(email_ID, callback) {
     var name = det_arr[2].trim();
     if (email_ID.trim() == email_ID_file) {
       console.log("Student ID: " + student_id);
-      var d = callback(student_id);
+      var d = Array.from(callback(student_id, superman));
       console.log("Success!");
     }
   }
 }
 
-function student_find_activities(student_ID) {
-  var total_days = 0;
-  var tda = [];
-  var data_p_array = [];
-  var complete_det = [];
+function compute_days_worked(data, total_days){
+    var complete_det = [];
+    var act_data_time = data.split('?');
+    act_data_time.forEach(function(v){
+        var activity = v.split('&');
+        var activity_name = activity[0];
+        var activity_dates = activity[1].split('%');
+        //console.log(activity_dates);
+        activity_dates.forEach(function(vl){
+            total_days.push(vl);
+        });
+         complete_det.push(activity_name, activity_dates, activity_dates.length);
+    
+    });
+    return complete_det;
+}
+
+function student_find_activities(student_ID, batman) {
+  var computed_data = [];
   var total_days = [];
   var lrs = new LineReaderSync("std_act_2.txt");
   while (true) {
@@ -35,27 +49,21 @@ function student_find_activities(student_ID) {
       if (line.includes(student_ID)) {
         var std_id_data = line.split("***");
         var std_id = std_id_data[0];
-        var act_data_time = std_id_data[1].split('?');
-        act_data_time.forEach(function(v){
-            var activity = v.split('&');
-            var activity_name = activity[0];
-            var activity_dates = activity[1].split('%');
-            activity_dates.forEach(function(vl){
-                total_days.push(vl);
-            });
-            complete_det.push([activity_name, activity_dates, activity_dates.length]);
-        });
+        console.log(std_id);
+        computed_data.push(batman(std_id_data[1], total_days));
+        }
         
       }
     }
     var total_unique_days = Array.from(new Set(total_days)).length;
-    complete_det.unshift(total_unique_days)
-    console.log(complete_det);
-    return complete_det;
-  }
+    console.log("Total unique days: ",total_unique_days);
+    computed_data[0].unshift(total_unique_days)
+    console.log(computed_data);
+    return computed_data[0];
 } 
-link_student_id("eng19cs0009.abhijeetkumar@gmail.com", student_find_activities);  
+
+link_student_id("eng19cs0009.abhijeetkumar@gmail.com", student_find_activities, compute_days_worked);  
 // module.exports = {
 //   lk: link_student_id,
 //   sfa: student_find_activities,
-// };
+//   cdw: compute_days_worked };
