7855fe83283cf40ea98d3a7fd69dc5b1f6808ea2
Deven-14 <deven.paramaj@gmail.com>
2021-05-03 20:50:57 +0530

    getinfo done

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -7,16 +7,19 @@ const port = 4200;
 const app = express();
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.set("view engine", "ejs");
-app.get("/", (req, res) => {
+
+app.get("/", (req, res) =>{
+    res.render("home.ejs");
+});
+
+app.post("/user", (req, res) => {
     var n = req.body.name;
     console.log(n);
-    console.log(req.body);
-    var url = document.URL;
-    var c = url.searchParams.get("name");
-    console.log(c);
-    console.log(getinfo.GetInfo(c));
-    res.render("home.ejs");
+    console.log(getinfo.GetInfo(n));
+    res.render("user.ejs");
 });
 app.listen(port, () => {
     console.log(`this log is working on ${port}`);
