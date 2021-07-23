8345d2b2b7084f12e1e04dc7497fd08ec7560675
sumedhdkme19 <72097989+sumedhdkme19@users.noreply.github.com>
2021-07-23 15:04:10 +0530

    Initial Commit

---
+++ data_collectors/2_create_folders.js
@@ -0,0 +1,28 @@
+  
+const fs = require('fs')
+const lineReaderSync = require('line-reader-sync')
+
+
+function make_directories()
+{
+    var lrs = new lineReaderSync("../data_files/classroom_details.csv")
+    while(true)
+    {
+        var line = lrs.readline();
+        if(line === null)
+        {
+            break;
+        }
+        var list = line.split(",")
+        let name = list[0]
+        if(list[1] !== "null")
+            name += "_"+list[1]
+        fs.mkdir(`../data_files/${name}`, (err)=> {
+            if(err)
+                console.log(err)
+            console.log(`Created File ${name}`)
+        })
+    }
+}
+
+make_directories()
